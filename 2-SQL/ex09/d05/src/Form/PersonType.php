476612527d690fb1a03e3c562e92b94021157a37
Elio PASTORET <elpastor@f4r1s6.clusters.42paris.fr>
2026-03-09 19:16:04 +0100

    ex09 finished ex10 ndy

---
+++ 2-SQL/ex09/d05/src/Form/PersonType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\BankAccount;
+use App\Entity\Person;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('name')
+            ->add('email')
+            ->add('enable')
+            ->add('birthdate')
+            ->add('maritalStatus')
+            ->add('bankAccount', EntityType::class, [
+                'class' => BankAccount::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Person::class,
+        ]);
+    }
+}
