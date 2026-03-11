951f8b06634a6544280188d700384ec6fb855f00
Elio PASTORET <elpastor@f4r4s9.clusters.42paris.fr>
2026-03-11 16:13:56 +0100

    2-SQL ex11 glr

--- 2-SQL/ex11/d05/src/Controller/E11Controller.php
+++ 2-SQL/ex11/d05/src/Controller/E11Controller.php
@@ -6,25 +6,37 @@ use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class E11Controller extends AbstractController
 {
     #[Route('/ex11', name: 'ex11')]
-    public function index(Connection $connection): Response
+    public function index(Request $request, Connection $connection): Response
     {
-        $users = $connection->fetchAllAssociative("
-            SELECT p.id, p.username, p.name, p.birthdate,
-                   b.account_number,
-                   a.city
-            FROM persons p
-            JOIN bank_accounts b ON p.id = b.person_id
-            JOIN addresses a ON p.id = a.person_id
-            ORDER BY p.name ASC
-        ");
+        $sql = "
+            CREATE TABLE person (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255),
+                name VARCHAR(255),
+                email VARCHAR(255),
+                birthdate DATETIME
+        ";
+        $sort = $request->query->get('sort');
+
+        if (!in_array($sort, ['name', 'birthdate', null])) {
+            $sort = null;
+        }
+
+        if ($sort === 'name') {
+            $sql .= " ORDER BY p.name ASC";
+        } elseif ($sort === 'birthdate') {
+            $sql .= " ORDER BY p.birthdate ASC";
+        }
+
+        $person = $connection->fetchAllAssociative($sql);
 
         return $this->render('ex11/index.html.twig', [
-            'users' => $users
+            'person' => $person
         ]);
     }
 }
