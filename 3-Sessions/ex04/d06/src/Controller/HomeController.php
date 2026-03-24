c7e6296703a8d60bf6047e08b10ccefbb56329b2
Elio PASTORET <elpastor@f6r6s3.clusters.42paris.fr>
2026-03-24 20:24:01 +0100

    3-S ex03-04 done

---
+++ 3-Sessions/ex04/d06/src/Controller/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(SessionInterface $session): Response
+    {
+        $animals = ['dog', 'cat', 'lion', 'tiger', 'fox', 'bear', 'wolf'];
+
+        $now = time();
+        $anonymousName = $session->get('anonymous_name');
+        $last = $session->get('last_request_time', $now);
+
+        if (!$anonymousName || ($now - $last) > 60) {
+            $animal = $animals[array_rand($animals)];
+            $anonymousName = 'Anonymous ' . $animal;
+            $session->set('anonymous_name', $anonymousName);
+        }
+
+        $session->set('last_request_time', $now);
+        $seconds_since_last = $now - $last;
+
+        return $this->render('home/index.html.twig', [
+            'anonymous_name' => $anonymousName,
+            'seconds_since_last' => $seconds_since_last,
+        ]);
+    }
+}
